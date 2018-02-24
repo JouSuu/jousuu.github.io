1990d609d116e5d11af3d3efcdfb638d9314f6a9
Jou_Suu <jousuu2069@gmail.com>
2018-02-25 00:49:34 +0900

    test

--- ultrasimple/sketch.js
+++ ultrasimple/sketch.js
@@ -5,8 +5,8 @@ window.onload = function()
 
 
 // Define
-var _W = 450;
-var _H = 450;
+var _W = 512;
+var _H = 512;
 var _StartTime = Date.now();
 var FPS = 60;
 var abs = Math.abs;
@@ -274,10 +274,20 @@ var texCoord =
 
 // Init
 var canvas = document.querySelector('#glcanvas');
+canvas.addEventListener('mousemove', onMouseMove, false);
 canvas.addEventListener('mousedown', onMouseDown, false);
 canvas.addEventListener('mouseup', onMouseUp, false);
-canvas.addEventListener('mousemove', onMouseMove, false);
 canvas.addEventListener('mousewheel', onMouseWheel, false);
+canvas.addEventListener('touchmove', function(event) {
+    mouseX = event.changedTouches[0].pageX;
+    mouseY = event.changedTouches[0].pageY;
+}, false);
+canvas.addEventListener('touchstart', function(event) {
+    event.preventDefault(); // ignore move
+}, false);
+
+
+
 
 canvas.width = _W;
 canvas.height = _H;
@@ -378,44 +388,108 @@ image_wall.onload = function()
 /*--------------------------------*/
 // plane start
 /*--------------------------------*/
+var dimension = 256;
+var texture_size = 1;
+var stride = texture_size/(dimension-1);
+var stride_texture = 1/(dimension-1);
 
-var vertices_plane = 
-[
-    0,0,0,
-    1,0,0,
-    2,0,0,
+var vertices_plane = [];
+for(var y=0;y<dimension;y++)
+{
+    for(var x=0;x<dimension;x++)
+    {
+        vertices_plane.push(x*stride);
+        vertices_plane.push(y*stride);
+        vertices_plane.push(0);
+    }
+}
 
-    0,-1,0,
-    1,-1,0,
-    2,-1,0,
+var indexes_plane = [];
+for(var y=0;y<dimension-1;y++)
+{
+    for(var x=0;x<dimension-1;x++)
+    {
+        var first_index = x+y*dimension;
+        var second_index = x+(y+1)*dimension;
+        var third_index = x+(y+1)*dimension +1;
+        indexes_plane.push(first_index);
+        indexes_plane.push(second_index);
+        indexes_plane.push(third_index);
 
-    0,-2,0,
-    1,-2,0,
-    2,-2,0,
-];
+        var fourth_index = first_index;
+        var fifth_index = third_index;
+        var sixth_index = first_index+1;
+        indexes_plane.push(fourth_index);
+        indexes_plane.push(fifth_index);
+        indexes_plane.push(sixth_index);
+    }
+}
 
-var indexes_plane =
-[
-    0,3,4,
-    0,4,1,
-    1,4,5,
-    1,5,2,
-    3,6,7,
-    3,7,4,
-    4,7,8,
-    4,8,5,
-];
+var texcoords_plane = [];
+for(var y=0;y<dimension;y++)
+{
+    for(var x=0;x<dimension;x++)
+    {
+        texcoords_plane.push(stride_texture*x);
+        texcoords_plane.push(stride_texture*y);
+    }
+}
 
 var vs_plane = createShader(gl,"PlaneShader-vs",gl.VERTEX_SHADER);
 var fs_plane = createShader(gl,"PlaneShader-fs",gl.FRAGMENT_SHADER);
 var prg_plane = createProgram(gl,vs_plane,fs_plane);
 var buff_plane = createBuffer(vertices_plane);
 var indexBuff_plane = createIndexBuffer(indexes_plane);
+var frameBuff_plane = createFrameBufferAndTexture(512,512);
+var planeTexture;
+var planeTexCoordBuff;
+
 /*--------------------------------*/
 // plane end
 /*--------------------------------*/
 
 
+
+
+
+
+/*--------------------------------*/
+// wave start
+/*--------------------------------*/
+var vertShader_input = createShader(gl,"input-vs",gl.VERTEX_SHADER);
+var fragShader_input = createShader(gl,"input-fs",gl.FRAGMENT_SHADER);
+var prg_input = createProgram(gl,vertShader_input,fragShader_input);
+
+var vertShader_wave = createShader(gl,"wave-vs",gl.VERTEX_SHADER);
+var fragShader_wave = createShader(gl,"wave-fs",gl.FRAGMENT_SHADER);
+var prg_wave = createProgram(gl,vertShader_wave,fragShader_wave);
+
+var vertices =
+[
+    -1 , 1, 0,
+    -1 ,-1, 0,
+     1 , 1, 0,
+     1 ,-1, 0,
+];
+
+var texCoord =
+[
+    0.0,1.0,
+    0.0,0.0,
+    1.0,1.0,
+    1.0,0.0,
+];
+
+var vertBuff = createBuffer(vertices);
+var texCoordBuff = createBuffer(texCoord);
+
+var frameBuff_input = createFrameBufferAndTexture(_W,_H);
+var frameBuff_current = createFrameBufferAndTexture(_W,_H);
+var frameBuff_back = createFrameBufferAndTexture(_W,_H);
+var frameBuffs = [frameBuff_current,frameBuff_back];
+/*--------------------------------*/
+// wave end
+/*--------------------------------*/
 
 
 loading();
@@ -432,8 +506,24 @@ function loading()
             ready = false;
     }
 
+    // when image ready
     if(ready)
     {
+        // create plane texture
+        planeTexCoordBuff = createBuffer(texcoords_plane);
+        gl.activeTexture(gl.TEXTURE0);
+
+        planeTexture = gl.createTexture();
+        gl.bindTexture(gl.TEXTURE_2D, planeTexture);
+        gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, image);
+        gl.generateMipmap(gl.TEXTURE_2D);
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+
+
+
         cancelAnimationFrame(callBackId);
         update();
         return;
@@ -451,11 +541,11 @@ function update()
 
     // camera
     var eyePos = vec3.create();
-    vec3.set(eyePos,2.5,2,3);
-    //vec3.set(eyePos,6.5,7,2.5);
+    //vec3.set(eyePos,2.5,2,3);
+    vec3.set(eyePos,-2.5,3,-0.5);
     var centerPos = vec3.create();
-    vec3.set(centerPos,0,0,0);
-    //vec3.set(centerPos,3,3,0);
+    //vec3.set(centerPos,0,0,0);
+    vec3.set(centerPos,-2.5,1,1.5-1.0);
 
     // wheel actions
     vec3.add(eyePos,eyePos,[_wheelMoveOffs[0]*0.01,-_wheelMoveOffs[1]*0.01,0]);
@@ -482,12 +572,75 @@ function update()
     gl.clearColor(BG_COLOR[0],BG_COLOR[1],BG_COLOR[2],0.0);
     gl.clear(gl.COLOR_BUFFER_BIT);
 
+/*
     box_old.draw(vp,lightDir);
     box_wall.draw(vp,lightDir);
     gl.bindTexture(gl.TEXTURE_2D, frameBuffAndTex.texture);
     // draw box
     //box_grass.texture = frameBuffAndTex.texture;
     box_grass.draw(vp,lightDir);
+*/
+
+
+
+    // Wave
+    /*------------------------*/
+    // draw input to input texture
+    gl.bindFramebuffer(gl.FRAMEBUFFER, frameBuff_input.frameBuffer);
+    gl.useProgram(prg_input);
+    gl.viewport(0, 0, _W, _H);
+    gl.clearColor(0.0,0.0,0.0,0.0);
+    gl.clear(gl.COLOR_BUFFER_BIT);
+
+    setAttribute(gl,prg_input,vertBuff,"v_pos",3);
+    gl.uniform2fv(gl.getUniformLocation(prg_input,'u_mousePos'), [mouseX,mouseY]);
+    gl.uniform2fv(gl.getUniformLocation(prg_input,'u_resolution'), [_W,_H]);
+    gl.uniform1i(gl.getUniformLocation(prg_input,"u_click"), mouseClick);
+    gl.uniform1f(gl.getUniformLocation(prg_input,"u_time"), dt);
+
+    gl.drawArrays(gl.TRIANGLE_STRIP,0,vertices.length/3);
+    gl.flush();
+    gl.bindFramebuffer(gl.FRAMEBUFFER, null);
+
+
+    for(var i=0;i<1;i++)
+    {
+        var idxs = getNextIndexes(cnt);
+        cnt++;
+        var currentBuff = frameBuffs[idxs.current];
+        var backBuff = frameBuffs[idxs.back];
+
+
+        /*------------------------*/
+        // draw wave to current texture
+        gl.bindFramebuffer(gl.FRAMEBUFFER, currentBuff.frameBuffer);
+        gl.useProgram(prg_wave);
+        gl.viewport(0, 0, _W, _H);
+        gl.clearColor(0.0,0.0,0.0,0.0);
+        gl.clear(gl.COLOR_BUFFER_BIT);
+        setAttribute(gl,prg_wave,vertBuff,"v_pos",3);
+        gl.uniform2fv(gl.getUniformLocation(prg_wave,'u_resolution'), [_W,_H]);
+        gl.uniform2fv(gl.getUniformLocation(prg_wave,'mouse'), [mouseX,mouseY]);
+        gl.uniform1f(gl.getUniformLocation(prg_wave,"u_time"), dt);
+
+        // from input texture
+        gl.activeTexture(gl.TEXTURE0);
+        gl.bindTexture(gl.TEXTURE_2D, frameBuff_input.texture);
+        gl.uniform1i(gl.getUniformLocation(prg_wave, 'texture_input'), 0);
+        setAttribute(gl,prg_wave,texCoordBuff,"a_texCoord_input",2);
+
+        gl.activeTexture(gl.TEXTURE1);
+        gl.bindTexture(gl.TEXTURE_2D, backBuff.texture);
+        gl.uniform1i(gl.getUniformLocation(prg_wave, 'texture_back'), 1);
+
+        gl.drawArrays(gl.TRIANGLE_STRIP,0,vertices.length/3);
+        gl.flush();
+        gl.bindTexture(gl.TEXTURE_2D, null);
+        gl.bindFramebuffer(gl.FRAMEBUFFER, null);
+        /*------------------------*/
+
+    }
+    /*------------------------*/
 
 
 
@@ -497,10 +650,19 @@ function update()
     gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuff_plane);
     var world_plane = mat4.create();
     var wvp_plane = mat4.create();
+    mat4.scale(world_plane,world_plane,[4,4,4]);
+    mat4.rotate(world_plane,world_plane,glMatrix.toRadian(90),[1,0,0])
+    mat4.rotate(world_plane,world_plane,glMatrix.toRadian(180),[0,1,0])
 
     mat4.multiply(world_plane,vp,world_plane);
     var u_wvp_plane = gl.getUniformLocation(prg_plane, 'u_wvp');
     gl.uniformMatrix4fv(u_wvp_plane,false,world_plane);
+
+    // texture
+    gl.activeTexture(gl.TEXTURE0);
+    gl.bindTexture(gl.TEXTURE_2D, currentBuff.texture);
+    gl.uniform1i(gl.getUniformLocation(prg_plane, 'texture'), 0);
+    setAttribute(gl,prg_plane,planeTexCoordBuff,"a_texCoord",2);
 
     gl.uniform3f(gl.getUniformLocation(prg_plane,"u_color"),0,1,0);
     setAttribute(gl,prg_plane,buff_plane,"v_pos",3);
@@ -587,11 +749,17 @@ function createFrameBufferAndTexture(w,h)
         gl.bindFramebuffer(gl.FRAMEBUFFER, frameBuffer);
 
         var texture = gl.createTexture();
+        gl.activeTexture(gl.TEXTURE0);
         gl.bindTexture(gl.TEXTURE_2D, texture);
         
         gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, w, h, 0, gl.RGBA, gl.UNSIGNED_BYTE, null);
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
         gl.framebufferTexture2D(gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT0, gl.TEXTURE_2D, texture, 0);
         
+        // unbind
         gl.bindTexture(gl.TEXTURE_2D, null);
         gl.bindFramebuffer(gl.FRAMEBUFFER, null);
         
@@ -607,6 +775,15 @@ function setAttribute(gl,prg,buffer,name,stride)
     var al = gl.getAttribLocation(prg, name);
     gl.vertexAttribPointer(al,stride,gl.FLOAT,false,0,0);
     gl.enableVertexAttribArray(al);
+}
+
+function getNextIndexes(i)
+{
+    //0,1
+    //1,0
+    var current = i%2;
+    var back = (i+1)%2;
+    return{current:current,back:back};
 }
 
 function arrow(pos,target,scale,color)
@@ -645,6 +822,8 @@ var __mouseDownPos = [0,0];
 var __wheelDownNow = false;
 var __wheelDownPos = [0,0];
 function onMouseMove(e) {
+    mouseX = e.clientX;
+    mouseY = e.clientY;
     if(__mouseDownNow)
     {
 
